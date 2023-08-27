4dd8dd808224a91dba5079bad0797f7c7cd620a3
Mohammad Dorri <140049110+PG6AW@users.noreply.github.com>
2023-08-27 15:50:10 +0330

    Create AdadChapKon_QueraOrg(Integer_output).py
    Quera.org

---
+++ AdadChapKon_QueraOrg(Integer_output).py
@@ -0,0 +1,12 @@
+a = int(input())
+for i in str(a):
+    b = []
+    print(f"{int(i)}: ",end = (""))
+    if int(i) > 0 :
+     for j in range(int(i)):
+            b.append(i)
+    elif int(i) == 0 :
+        print()        
+    output = "".join(b)
+    if int(i) > 0 :
+     print(int(output))
